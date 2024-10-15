3e24e05c59bef7131b5bbc72399a023be571a9b7
Hanna-Mari Kielenniva <h.kielenniva@gmail.com>
2024-10-15 12:10:18 +0300

    valmis

--- App.js
+++ App.js
@@ -1,12 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import * as Contacts from 'expo-contacts';
+import * as SMS from 'expo-sms';
+import { useState } from 'react';
 
 export default function App() {
+  const [contact, setContact] = useState({});
+
+const getContacts = async () => {
+  const { status } = await Contacts.requestPermissionsAsync(); //kysyy lupaa kameran käyttööna
+  if (status === 'granted') {
+    const { data } = await Contacts.getContactsAsync(
+      { fields: [Contacts.Fields.PhoneNumbers] }
+    );
+    if (data.length > 0) {
+      setContact(data[0]);
+    }
+  }
+}
+
+const sendSms = async () => {
+  const isSMSAvailable = await SMS.isAvailableAsync();
+
+  if (isSMSAvailable && contact.phoneNumbers.length > 0) {
+    const { result } = await SMS.sendSMSAsync([contact.phoneNumbers[0].number], 
+      'Hello ' + contact.name);
+  }
+}
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Text>{contact.name}</Text>
+    <Button title="Get Contact" onPress={getContacts} />
+    <Button title="Send SMS" onPress={sendSms} />
+  </View>
   );
 }
 
